f1701af7b76c7d393f581e0a55b38b13da08fbd9
Min Thura Zaw <minthurazaw619@gmail.com>
2023-11-29 20:02:43 +0700

    methods

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,16 @@
-class Main {
+import Methods.showPrice
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    println("-------------------------------")
+
+    println(showPrice(30.5, 2))
+
+
+    println("-------------------------------")
+  }
+
+
 
 }
