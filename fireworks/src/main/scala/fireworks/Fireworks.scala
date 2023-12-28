b68746c05ca702b270242b5806bdd73e95e60a97
Min Thura Zaw <minthurazaw619@gmail.com>
2023-12-28 10:41:15 +0700

    update functions

--- fireworks/src/main/scala/fireworks/Fireworks.scala
+++ fireworks/src/main/scala/fireworks/Fireworks.scala
@@ -162,7 +162,10 @@ case class Exploding(countDown: Int, particles: Particles) extends Firework:
    *       of this firework.
    */
   def next: Firework =
-    ???
+    if countDown > 0 then
+      copy(countDown - 1, particles.next)
+    else
+      Done
 
 end Exploding
 
@@ -209,16 +212,16 @@ case class Particle(horizontalSpeed: Double, verticalSpeed: Double, position: Po
     // should be the current value reduced by air friction
     // Hint: use the operation `Motion.drag`
     val updatedHorizontalSpeed: Double =
-      ???
+    Motion.drag(horizontalSpeed)
     // Vertical speed is subject to both air friction and gravity, its next
     // value should be the current value minus the gravity, then reduced by
     // air friction
     val updatedVerticalSpeed: Double =
-      ???
+    Motion.drag(verticalSpeed - Settings.gravity)
     // Particle position is updated according to its new speed
     val updatedPosition = Point(position.x + updatedHorizontalSpeed, position.y + updatedVerticalSpeed)
     // Construct a new particle with the updated position and speed
-    ???
+    Particle(updatedHorizontalSpeed, updatedVerticalSpeed, updatedPosition, color)
 
 end Particle
 
