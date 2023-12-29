f4d270f10242e714f76e9a341b7a0ed3c6806650
Min Thura Zaw <minthurazaw619@gmail.com>
2023-12-29 22:21:44 +0700

    functions

--- scala-practice/src/main/scala/Functions.scala
+++ scala-practice/src/main/scala/Functions.scala
@@ -5,13 +5,24 @@ object Functions extends App {
     result
   }
 
+  val renameColumns = (name:String) => name.toLowerCase()
+
   val endWithDotCom: String => Boolean = _.endsWith(".com")
+
+  val applyDiscount = (totalCost:Double, discount:Double) => totalCost - discount
+
 
   val result1 = increment.apply(2)
   println(result1)
 
   val result2 = endWithDotCom("minthura")
   println(result2)
+
+  val result3 = renameColumns("Name")
+  println(result3)
+
+  val result4 = applyDiscount(100, 4)
+  println(result4)
 
 
 }
