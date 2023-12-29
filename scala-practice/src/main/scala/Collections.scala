c9648cb446dd88810b00208982527256b6895dbe
Min Thura Zaw <minthurazaw619@gmail.com>
2023-12-29 22:03:42 +0700

    scala collections

---
+++ scala-practice/src/main/scala/Collections.scala
@@ -0,0 +1,10 @@
+import scala.collection.mutable
+
+object Collections extends App{
+
+  val myList = List.empty[Int]
+  val myAB = mutable.ArrayBuffer.empty[String]
+  val myMap = Map.empty[Int, String]
+
+
+}
