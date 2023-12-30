cbceeff7c9d3e55e17633240c4839c017abc742c
Min Thura Zaw <minthurazaw619@gmail.com>
2023-12-30 10:14:10 +0700

    collections

--- scala-practice/src/main/scala/Collections.scala
+++ scala-practice/src/main/scala/Collections.scala
@@ -6,5 +6,15 @@ object Collections extends App{
   val myAB = mutable.ArrayBuffer.empty[String]
   val myMap = Map.empty[Int, String]
 
+  val myList2 = 0 +: myList :+ 1
+  println(myList2)
+
+  // Tuples
+  val pair1:(Int, String) = 1 -> "Me"
+  val pair2:(Int, String) = 2 -> "You"
+
+  val myMap2: Map[Int, String] = Map(pair1, pair2)
+  println(myMap2)
+
 
 }
