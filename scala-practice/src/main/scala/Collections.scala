becdcc790f6c3dff2f2ae5544d99c3ac53b14fe5
Min Thura Zaw <minthurazaw619@gmail.com>
2023-12-30 10:43:29 +0700

    collections

--- scala-practice/src/main/scala/Collections.scala
+++ scala-practice/src/main/scala/Collections.scala
@@ -3,11 +3,14 @@ import scala.collection.mutable
 object Collections extends App{
 
   val myList = List.empty[Int]
-  val myAB = mutable.ArrayBuffer.empty[String]
+  var myAB = mutable.ArrayBuffer.empty[String]
   val myMap = Map.empty[Int, String]
 
   val myList2 = 0 +: myList :+ 1
   println(myList2)
+
+  myAB = myAB :+ "hello"
+  println(myAB :+ "hello")
 
   // Tuples
   val pair1:(Int, String) = 1 -> "Me"
@@ -15,6 +18,18 @@ object Collections extends App{
 
   val myMap2: Map[Int, String] = Map(pair1, pair2)
   println(myMap2)
+
+
+  def euclideanDivision(dividend: Int, divisor: Int): (Int, Int) =
+    val quotient = dividend / divisor
+    val remainder = dividend % divisor
+    (quotient, remainder)
+
+  val result = euclideanDivision(10,3)
+  print(result(0))
+
+  val (q,r) = euclideanDivision(10,4)
+  println(r)
 
 
 }
