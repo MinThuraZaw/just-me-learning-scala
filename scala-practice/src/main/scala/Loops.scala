4dca2bea8d35358050113062b4a1a0c3c9a7987c
Min Thura Zaw <minthurazaw619@gmail.com>
2024-01-06 17:14:49 +0700

    loops

--- scala-practice/src/main/scala/Loops.scala
+++ scala-practice/src/main/scala/Loops.scala
@@ -1,3 +1,5 @@
+case class Task(name:String, duration:Int, requirements:List[Task])
+
 object Loops extends App {
 
   println("Loops")
@@ -6,7 +8,32 @@ object Loops extends App {
     (1 to n).foldLeft(1)((result, x) => result * x)
   }
 
-
   println(factorial(4))
+  println((1 to 10))
+
+  var n = 10
+  var i = 0
+  while (i < n) {
+    // println(i)
+    i += 1
+  }
+
+  val csSetup = Task("cs setup", 4, Nil)
+  val ide = Task("ide", 3, Nil)
+  val hack = Task("hack", 8, List(csSetup, ide))
+  val deploy = Task("deploy", 5, List(hack))
+
+  def totalDuration(task: Task): Int = {
+    val reqMaxTotalDuration =
+      task.requirements
+        .map(totalDuration)
+        .maxOption
+        .getOrElse(0)
+
+    task.duration + reqMaxTotalDuration
+
+  }
+
+  println(totalDuration(deploy))
 
 }
