f60875b7d70974fd029b8476a9fe76365da4a09f
Min Thura Zaw <minthurazaw619@gmail.com>
2024-01-07 10:55:28 +0700

    democracy assignment

--- democracy/src/main/scala/democracy/MajorityJudgement.scala
+++ democracy/src/main/scala/democracy/MajorityJudgement.scala
@@ -31,7 +31,7 @@ object Grade:
    * - `apply` to select an element at a specific index.
    */
   def median(grades: Seq[Grade]): Grade =
-    ???
+    grades.sortBy(_.ordinal).apply(grades.size / 2)
 end Grade
 
 /**
@@ -66,78 +66,41 @@ case class Election(description: String, candidates: Set[Candidate]):
     assert(ballots.nonEmpty)
     assert(ballots.forall(_.grades.keySet == candidates))
 
-    // Re-structure the data to get all the grades assigned to
-    // each candidate by all the voters
-
-    // First step: use the operation `flatMap` to flatten the ballots
-    // into a single sequence containing the grades assigned to each
-    // candidate by the voters.
     val allGrades: Seq[(Candidate, Grade)] =
-      ???
+      ballots.flatMap(_.grades.map((candidate,grade) => (candidate,grade)))
 
-    // Second step: use the operation `groupMap` to transform the
-    // collection of pairs of `(Candidate, Grade)` into a `Map`
-    // containing all the grades that were assigned to a given
-    // `Candidate`.
     val gradesPerCandidate: Map[Candidate, Seq[Grade]] =
-      ???
+      allGrades.groupMap(_._1)(_._2)
 
     findWinner(gradesPerCandidate)
   end elect
 
-  /**
-   * @return The winner of this election, according to the Majority Judgement
-   *         voting process.
-   *
-   * @param gradesPerCandidate The grades that have been assigned to each
-   *                             candidate by the voters.
-   */
   def findWinner(gradesPerCandidate: Map[Candidate, Seq[Grade]]): Candidate =
-    // In case all the candidates have an empty collection of grades (this
-    // can happen because of the tie-breaking algorithm, see below), the winner
-    // is chosen by lottery from among the candidates.
+
     if gradesPerCandidate.forall((candidate, grades) => grades.isEmpty) then
       val candidatesSeq = gradesPerCandidate.keys.toSeq
       val randomIndex   = util.Random.between(0, candidatesSeq.size)
       candidatesSeq(randomIndex)
     else
-      // Otherwise, find the highest median grade assigned to a candidate.
-      // Use the operation `values` to select the collections of grades,
-      // then use the operation `filter` to keep only the non empty grades,
-      // then use the operation `map` to compute the median value of each collection
-      // of grades, and finally use the operation `maxBy` to find the highest
-      // median grade.
       val bestMedianGrade: Grade =
-        ???
+        gradesPerCandidate
+          .values
+          .filter(_.nonEmpty)
+          .map(grade => Grade.median(grade))
+          .maxBy(_.ordinal)
 
-      // Use the operation `filter` to select all the candidates that got the
-      // same best median grade (as the case may be)
       val bestCandidates: Map[Candidate, Seq[Grade]] =
-        ???
+        gradesPerCandidate
+          .filter((_, listGrade) => Grade.median(listGrade) == bestMedianGrade)
 
-      // In case only one candidate got the best median grade, it’s the winner!
       if bestCandidates.size == 1 then
-        // Use the operation `head` to retrieve the only element
-        // of the collection `bestCandidates`
-        ???
+        bestCandidates.head._1
       else
-        // Otherwise, there is a tie between several candidates. The tie-breaking
-        // algorithm is the following:
-        // “If more than one candidate has the same highest median-grade, the winner is
-        // discovered by removing (one-by-one) any grades equal in value to the shared
-        // median grade from each tied candidate's total. This is repeated until only one
-        // of the previously tied candidates is currently found to have the highest
-        // median-grade.” (source: https://en.wikipedia.org/wiki/Majority_judgment)
-  
-        // Use the operation `map` to transform each element of the `bestCandidates`.
-        // And use the operation `diff` to remove one `bestMedianGrade` from the
-        // grades assigned to the candidates.
         val bestCandidatesMinusOneMedianGrade: Map[Candidate, Seq[Grade]] =
-          ???
-  
-        // Finally, call `findWinner` on the reduced collection of candidates,
-        // `bestCandidatesMinusOneMedianGrade`.
-        ???
+          bestCandidates.map((candidate, grade) => candidate -> grade.diff(List(bestMedianGrade)))
+
+        findWinner(bestCandidatesMinusOneMedianGrade)
+
   end findWinner
 
 end Election
