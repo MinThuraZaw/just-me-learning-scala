7602b35513d6957f374c3c60fc97b57b8a2b694d
Min Thura Zaw <minthurazaw619@gmail.com>
2024-01-10 21:15:54 +0700

    update todo list app

--- todo/src/main/scala/todo/InMemoryModel.scala
+++ todo/src/main/scala/todo/InMemoryModel.scala
@@ -34,19 +34,21 @@ object InMemoryModel extends Model:
 
   def create(task: Task): Id =
     val id = idGenerator.nextId()
+    idStore += id -> task
     id
 
   def read(id: Id): Option[Task] =
     idStore.get(id)
 
   def complete(id: Id): Option[Task] =
-    None
+    update(id)(task => task.copy(state = State.completedNow))
 
   def update(id: Id)(f: Task => Task): Option[Task] =
     idStore.updateWith(id)(opt => opt.map(f))
 
   def delete(id: Id): Boolean =
     var found = false
+    idStore.remove(id)
     found
 
   def tasks: Tasks =
