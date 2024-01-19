7edcd038a7a634925e9661994e9af182c6b970b1
Min Thura Zaw <minthurazaw619@gmail.com>
2024-01-19 22:20:10 +0700

    Create ExtensionMethods.scala

---
+++ scala-practice/src/main/scala/ExtensionMethods.scala
@@ -0,0 +1,26 @@
+class ExtensionMethods {
+
+}
+
+// extension not directly supported in scala 2
+trait Ordering[A]{
+  def compare(x: A, y: A): Int
+
+}
+
+// implicit conversion
+sealed trait Json
+case class JNumber(value: BigDecimal) extends Json
+case class JString(value: String) extends Json
+case class JBoolean(value: Boolean) extends Json
+case class JArray(value: List[Json]) extends Json
+case class JObject(fields: (String, Json)*) extends Json
+
+object Json{
+  def obj(fields: (String, JsonField)* ): Json = {
+    // JObject(fields.map(_.json)*)
+
+  }
+
+  case class JsonField(json: Json)
+}
