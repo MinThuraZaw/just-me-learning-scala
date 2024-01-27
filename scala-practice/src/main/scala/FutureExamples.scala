1cec7f885394a2b36c2c999ce405891584fc13a1
Min Thura Zaw <minthurazaw619@gmail.com>
2024-01-27 11:33:04 +0700

    Future examples

---
+++ scala-practice/src/main/scala/FutureExamples.scala
@@ -0,0 +1,23 @@
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+case class User(first_name: String, last_name: String)
+
+object FutureExamples extends App {
+
+  def bcrypt(saltRound: Int, password: String): Future[Seq[Byte]] = {
+    ???
+  }
+
+  def insertUser(login: String, passwordHash: Seq[Byte]): User = {
+    ???
+  }
+
+  def hashPasswordAndInsert(name: String, password: String): Future[User] = {
+    bcrypt(10, password).map(passwordHash => insertUser(name, passwordHash))
+  }
+
+  println("calling bcrypt")
+  bcrypt(10, "paerendjfer").map(hash => println(s"Result : $hash"))
+}
+
